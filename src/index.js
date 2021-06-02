85894f6bb4c83a7e9afb7fc918ac52c4cc666f6b
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-06-02 20:09:35 -0300

    05- Tipos de parâmetros

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/courses', (req, res) => {
+    const {page, order} = req.query;
+    console.log(page);
+    console.log(order);
     return res.json(['curso 1', 'curso 2', 'curso 3']);
 });
 
 app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
     return res.json(['curso 1', 'curso 2', 'curso 3', 'curso 4']);
 });
 
 app.put('/courses/:id', (req, res) => {
+    const { id } = req.params;
+    console.log(id);
     return res.json(['curso 6', 'curso 2', 'curso 3', 'curso 4']);
 });
 
