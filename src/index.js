f41ccabd23035a29dd7b482011ec6dce28908553
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-06-02 17:57:12 -0300

    02- Adicionando o Nodemon na aplicação

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.json({message: "Hello World"});
+    return res.json({message: "Hello World!!!"});
 });
 
 app.listen(3333);
