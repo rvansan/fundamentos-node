61b0877945b7c27c245e7f866f015ae66de10a07
Rafael Vansan <rafaelvansan@MacBook-Air-de-Rafael.local>
2021-06-02 18:07:29 -0300

    03- Utilizando os métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({message: "Hello World!!!"});
+app.get('/courses', (req, res) => {
+    return res.json(['curso 1', 'curso 2', 'curso 3']);
+});
+
+app.post('/courses', (req, res) => {
+    return res.json(['curso 1', 'curso 2', 'curso 3', 'curso 4']);
+});
+
+app.put('/courses/:id', (req, res) => {
+    return res.json(['curso 6', 'curso 2', 'curso 3', 'curso 4']);
+});
+
+app.patch('/courses/:id', (req, res) => {
+    return res.json(['curso 6', 'curso 7', 'curso 3', 'curso 4']);
+});
+
+app.delete('/courses/:id', (req, res) => {
+    return res.json(['curso 6', 'curso 7', 'curso 4']);
 });
 
 app.listen(3333);
